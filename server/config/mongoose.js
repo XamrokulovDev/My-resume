b83fc29547a74f84d88acaa3c6121c3f9263e690
ulfatjon <xamrokulovulfatjon2005@gmail.com>
2025-03-03 13:42:09 +0500

    The project is started.

---
+++ server/config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+    try {
+        const conn = mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }catch (error) {
+        console.error(`Error in connecting to MongoDB: ${error.message}`);
+        exit(1);
+    }
+}
+
+connectDb();
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const connectDb = require('./config/mongoose');
+
+// PORT and Listening to the server
+const PORT = process.env.PORT;
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }catch (error) {
+        console.error(`Error in starting the server: ${error.message}`);
+    }
+}
+start();
